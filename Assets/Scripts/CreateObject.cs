951c7791386a283a0800fe2cef9dfc0dfb4f7a42
TD6370 <td6370@yandex.ru>
2018-09-29 21:32:22 +0400

    generic object Ufo

---
+++ Assets/Scripts/CreateObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObject : MonoBehaviour {
+
+    //Coroutine createObject;
+    public GameObject prefabUfo;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(CreateObjectUfo());
+    }
+
+    IEnumerator CreateObjectUfo()
+    {
+        int coutUfoReal = 0;
+
+        while (true)
+        {   
+            GameObject[] listPrefabUfo = GameObject.FindGameObjectsWithTag("Ufak") ;//  = gameObject.CompareTag ("Ufak")) ;
+            coutUfoReal = listPrefabUfo.Length;
+            if(coutUfoReal<10)
+            {
+                if (coutUfoReal == 0) coutUfoReal = 2;
+                //if(listPrefabUfo.Length>0){
+                //    otherUfo = listPrefabUfo[0];
+                //}
+                
+                GameObject newUfo = (GameObject)Instantiate(prefabUfo);
+                int add = (coutUfoReal * 1);
+                newUfo.transform.position = new Vector2(prefabUfo.transform.position.x, prefabUfo.transform.position.y - add);
+                //newUfo.MovePosition(newUfo.position + movement * speed * Time.deltaTime);
+
+                //print(newUfo.transform.position.ToString()); //Консоль
+                Debug.Log(newUfo.transform.position.ToString());//Дебаг
+                Debug.Log("Cout Ufo Real =" + coutUfoReal.ToString());//Дебаг
+            }
+            yield return new WaitForSeconds(3);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/MovementUfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementUfo : MonoBehaviour {
+
+    Coroutine moveObject;
+
+	// Use this for initialization
+	void Start () {
+	    //Coroutine moveObject =StartCoroutine(Move
+        moveObject = StartCoroutine(MoveObject());
+	}
+
+    IEnumerator MoveObject()
+    {
+        int distantion = 0;
+        float x = transform.position.x;
+        int compas=1;
+
+        while (true)
+        {
+            distantion++;
+            if (distantion > 200)
+            {
+                distantion = 0;
+                compas *= -1;
+                //yield break;
+            }
+
+            transform.Translate(compas * Time.deltaTime, 0, 0);
+            //transform.Translate(1, 0, 0);
+            yield return null;
+            //yield return new WaitForSeconds(1);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+
+}
